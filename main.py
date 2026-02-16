8f6aba91c2ea193d0409a37702f6288fa099cb1f
mgromel <m.gromelski@qsarlab.com>
2026-02-16 13:11:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ st.markdown(
 
     .footer.light {{
         background: #f9f9f9;
-        border-top-color: #878787;
+        border-top-color: #d6d4d4;
     }}
     .footer.dark {{
         background: #0e1117;
